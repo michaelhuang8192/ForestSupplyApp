6ba3b2047f062f9c56d05fb30b3b0048a6bd0e19
pk <huienhuang@gmail.com>
2016-11-27 19:27:15 -0800

    add db template and change logger settings

--- server/lib/Logger.js
+++ server/lib/Logger.js
@@ -2,10 +2,11 @@ var gCfg = require('../config');
 var gWinston = require('winston');
 
 var transports = [
-	new gWinston.transports.Console()
 ];
 if(gCfg.logFilename) {
 	transports.push(new (gWinston.transports.File)({ filename: gCfg.logFilename }));
+} else {
+	transports.push(new gWinston.transports.Console());
 }
 
 var logger = module.exports = new gWinston.Logger({ transports: transports });
