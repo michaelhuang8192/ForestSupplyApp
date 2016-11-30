3d1b73b10c5ef35a918245e8ee872a88dc31bb03
pk <huienhuang@gmail.com>
2016-11-30 04:16:14 -0800

    fixed server redirection

--- server/lib/Default.js
+++ server/lib/Default.js
@@ -5,3 +5,7 @@ var gRouter = exports.DefaultHandler = gExpress.Router();
 gRouter.all('/', (req, res, next) => {
 	res.redirect('/web/');
 });
+
+gRouter.all('/web/', (req, res, next) => {
+	res.redirect('/web/');
+});
